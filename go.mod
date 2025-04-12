b6470ab5a9a76c1382116a883375473c727a03b8
Mridul Dhiman <dhimanmridul91@gmail.com>
2025-04-13 04:24:33 +0530

    feat: made some changes

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/MridulDhiman/latest-manga-ping
 
 go 1.24.0
 
-require github.com/robfig/cron v1.2.0
-
 require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -6,19 +6,20 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/joho/godotenv"
-	"github.com/robfig/cron"
 )
 
 var (
-	manhwaList []string
-	mangaUrl   string
-	resp       *http.Response
-	err        error
+	manhwaList  []string
+	mangaUrl    string
+	resp        *http.Response
+	err         error
+	manhwaSrc   string
+	mangaPriSrc string
+	mangaSecSrc string
 )
-
-var manhwaSrc string
 
 type Manga struct {
 	mangaName         string
@@ -28,43 +29,92 @@ type Manga struct {
 
 func init() {
 	godotenv.Load()
-	manhwaSrc = os.Getenv("ASURA_SCANS_URL")
+	manhwaSrc = os.Getenv("MANHWA_SRC_URL")
 	manhwaList = []string{
 		os.Getenv("DRAGON_SLAYER_MANGA"),
+		os.Getenv("KNIGHT_KING_WHO_RETURNED_WITH_GOD_MANGA"),
 	}
+	mangaPriSrc = os.Getenv("MANGA_PRI_SRC_URL")
+	mangaSecSrc = os.Getenv("MANGA_SEC_SRC_URL")
 }
 
 func main() {
-	fmt.Println("hello world")
-	cron.New()
-	processManga(Manga{
-		mangaName:         "A Dragonslayer's Peerless Regression",
-		mangaType:         "manhwa",
-		latestChapterRead: 33,
-	})
-	select {}
+	mangaList := []Manga{
+		{
+			mangaName:         "A Dragonslayer's Peerless Regression",
+			mangaType:         "manhwa",
+			latestChapterRead: 33,
+		},
+		{
+			mangaName:         "The Knight King Who Returned with a God",
+			mangaType:         "manhwa",
+			latestChapterRead: 107,
+		},
+		{
+			mangaName:         "Kingdom",
+			mangaType:         "manga",
+			latestChapterRead: 345,
+		},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(mangaList))
+	for _, manga := range mangaList {
+		go func(manga Manga) {
+			defer wg.Done()
+			newChapterPublished := false
+			if manga.mangaType == "manhwa" {
+				newChapterPublished = processManhwa(manga)
+			} else if manga.mangaType == "manga" {
+				newChapterPublished = processManga(manga)
+			}
+
+			if newChapterPublished {
+				go notifyUser(manga)
+			}
+		}(manga)
+	}
+
+	wg.Wait()
 }
 
-func processManga(manga Manga) {
+func notifyUser(manga Manga) {
+	fmt.Println("manga: ", manga.mangaName)
+	fmt.Println("chapter: ", manga.latestChapterRead+1)
+	// TODO
+}
 
-	if manga.mangaType == "manhwa" {
-		mangaAvailable, manhwaName := isManhwaPresent(manga.mangaName)
-		if !mangaAvailable {
-			return
-		}
-		mangaUrl = formatMangaUrl(manhwaSrc, manga.latestChapterRead, manhwaName)
-		resp, err = http.Get(mangaUrl)
-	} else if manga.mangaType == "manga" {
-
+func processManhwa(manga Manga) bool {
+	mangaAvailable, manhwaName := isManhwaPresent(manga.mangaName)
+	if !mangaAvailable {
+		return false
 	}
+
+	return newMangaChapterAvailable(manhwaSrc, manga.latestChapterRead, manhwaName)
+}
+
+func processManga(manga Manga) bool {
+	manga.mangaName = formatMangaName(manga.mangaName)
+	return newMangaChapterAvailable(mangaPriSrc, manga.latestChapterRead, manga.mangaName) || newMangaChapterAvailable(mangaSecSrc, manga.latestChapterRead, manga.mangaName)
+}
+
+func newMangaChapterAvailable(mangaSrc string, latestChapterRead int, mangaName string) bool {
+	mangaUrl = formatMangaUrl(mangaSrc, latestChapterRead, mangaName)
+	resp, err = http.Get(mangaUrl)
 
 	if isValidResponse(resp, err) {
 		// latest chapter found
+		return true
 	}
+	return false
 }
 
 func formatManhwaName(mangaName string) string {
 	return strings.ReplaceAll(strings.Join(strings.Split(strings.ToLower(mangaName), " "), "-"), "'", "")
+}
+
+func formatMangaName(mangaName string) string {
+	return strings.Join(strings.Split(strings.ToLower(mangaName), " "), "-")
 }
 
 func isManhwaPresent(mangaName string) (bool, string) {
